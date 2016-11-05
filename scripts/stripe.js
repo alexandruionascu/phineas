483de240af5af933d4b3e5eeb9d436ad1d267e13
Alexandru Ionascu <alexandruionsacu.bd@gmail.com>
2016-11-05 12:31:25 +0200

    bot is showing stripe history

--- scripts/stripe.js
+++ scripts/stripe.js
@@ -1,9 +1,17 @@
 var stripe = require('stripe')('sk_test_ar94SyA9caWaloZxvJ3L2SJ1');
 
 module.exports = function(robot) {
-    robot.respond(/stripe charge/i, function(message) {
+    robot.hear(/show history/i, function(message) {
       message.reply("Your transactions are loading...")
-      stripe.balance.listTransactions({ limit: 3 }, function(err, transactions) {
+      stripe.balance.listTransactions(function(err, transactions) {
+        var history = [];
+        for(var i = 0; i < transactions.data.length; i++) {
+          history.push(transactions.data[i].description + " " + transactions.data[i].amount + "$");
+        }
+
+        for(var billIndex in history)
+          message.reply(history[billIndex]);
+
         message.reply(JSON.stringify(transactions));
       });
 
