23c4ffbb33822e72d5e5bea37300a9f6b6cd6f96
marius92mc <marius_92m@yahoo.com>
2016-11-05 23:14:15 +0200

    Added class to retrieve Stripe transactions

--- scripts/stripe.js
+++ scripts/stripe.js
@@ -1,14 +1,48 @@
 var stripe = require('stripe')('sk_test_ar94SyA9caWaloZxvJ3L2SJ1');
+//var transactions = require("./transactions.js")
+
+function TransactionWithTime(amount, description, created) {
+  this.amount = amount;
+  this.description = description;
+  this.created = created;
+}
+
+
+function StripeTransactions() {
+  //this.stripeTransactions = [];
+  this.history = [];
+
+  this.getStripeTransactions = function() {
+    stripe.balance.listTransactions(function(err, transactions) {
+      for (var i = 0; i < transactions.data.length; i++) {
+        this.history.push(new TransactionWithTime(
+          parseInt(transactions.data[i].amount),
+          transactions.data[i].description,
+          transactions.data[i].created)
+        );
+      }
+    });
+
+    return this.history;
+  }
+};
 
 module.exports = function(robot) {
     robot.hear(/show history/i, function(message) {
+
+      var x = new StripeTransactions();
+      history = x.getStripeTransactions();
+
+      for (var i = 0; i < history.length; i++) {
+        console.log(history[i].amount);
+      }
+
       message.reply("Your transactions are loading...")
       stripe.balance.listTransactions(function(err, transactions) {
         var history = [];
         for(var i = 0; i < transactions.data.length; i++) {
           history.push(transactions.data[i].description + " " + transactions.data[i].amount + "$");
         }
-
         for(var billIndex in history)
           message.reply(history[billIndex]);
 
