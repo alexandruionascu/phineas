0f1db8c203689b032a619fdd7f194ba472d6bcfe
marius92mc <marius_92m@yahoo.com>
2016-11-06 06:53:56 +0200

    Added links to image and sale example for items

--- scripts/wishlist.js
+++ scripts/wishlist.js
@@ -22,7 +22,24 @@ function computePriceForProductFromUrl(url, callback) {
     // Parse the document body
     $ = cheerio.load(body);
     var strProductInfos = $('li[id="result_0"]').text();
-    
+
+    var imgSrcUrl = $('li[id="result_0"] > div.s-item-container > ' +
+                      'div.a-fixed-left-grid > ' +
+                      'div.a-fixed-left-grid-inner > ' +
+                      'div.a-fixed-left-grid-col.a-col-left > ' +
+                      'div.a-row > ' +
+                      'div.a-column.a-span12.a-text-center > ' +
+                      'a.a-link-normal.a-text-normal > ' +
+                      'img.s-access-image.cfMarker').attr('src');
+
+    var productHref = $('li[id="result_0"] > div.s-item-container > ' +
+                      'div.a-fixed-left-grid > ' +
+                      'div.a-fixed-left-grid-inner > ' +
+                      'div.a-fixed-left-grid-col.a-col-left > ' +
+                      'div.a-row > ' +
+                      'div.a-column.a-span12.a-text-center > ' +
+                      'a.a-link-normal.a-text-normal').attr('href');
+
     var price = 0;
     for (var i = 0; i < strProductInfos.length; i++) {
       if (strProductInfos[i] == '$') {
@@ -36,7 +53,7 @@ function computePriceForProductFromUrl(url, callback) {
         break;
       }
     }
-    callback(price);
+    callback(price, imgSrcUrl, productHref);
   });
 };
 
@@ -44,8 +61,16 @@ function computePriceForProductFromUrl(url, callback) {
 module.exports = function(robot) {
   robot.respond(/price for (.*)/i, function(message) {
     computePriceForProductFromUrl(START_URL + message.match[1],
-      function(price) {
-        message.reply(price.toString() + "$");
+      function(price, imgSrc, productHref) {
+        if (typeof imgSrc !== "undefined") {
+          message.reply(imgSrc);
+        }
+        if (typeof productHref !== "undefined") {
+          message.reply("You could take a look at this one. \n" + productHref);
+        }
+        if (typeof price !== "undefined") {
+          message.reply(price.toString() + "$");
+        }
     });
   });
 };
