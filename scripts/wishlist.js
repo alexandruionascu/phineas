e0787b513657a3aa1b0e58529535bf248d94958f
marius92mc <marius_92m@yahoo.com>
2016-11-06 09:20:57 +0200

    Added Wishlist related classes

--- scripts/wishlist.js
+++ scripts/wishlist.js
@@ -5,6 +5,114 @@ var URL = require('url-parse');
 var START_URL = "https://www.amazon.com/s/ref=nb_sb_noss_2?url=search-alias%3Daps&field-keywords=";
 
 
+var Wishlist = function(name, href, imgSrcUrl, price, desiredPrice) {
+  this.name = name;
+  this.href = href;
+  this.imgSrcUrl = imgSrcUrl;
+  this.desiredPrice = desiredPrice;
+  this.time = Date.now();
+
+  /**
+   *  TODO
+   *    make pretty output
+   */
+  this.toString = function() {
+    return this.name + " " + this.href + " " + this.imgSrcUrl +
+      " " + this.desiredPrice;
+  }
+};
+
+
+var WishlistContainer = function() {
+  this.wishlistContainer = [];
+
+  this.count = function() {
+    return this.wishlistContainer.length;
+  }
+
+  this.pushToWishlist = function(name, href, imgSrcUrl, price, desiredPrice) {
+    if (name === "") {
+      return;
+    }
+    this.wishlistContainer.push(new Wishlist(name, href, imgSrcUrl, price,
+      desiredPrice));
+  }
+
+  this.getElementAtIndex = function(index) {
+    if (index < 0 || index >= this.wishlistContainer.length) {
+      return "";
+    }
+    return this.wishlistContainer[index];
+  }
+
+  this.popFromWishlistByName = function(name) {
+    if (name === "") {
+      return;
+    }
+    for (var i = 0; i < this.wishlistContainer.length; i++) {
+      if (this.wishlistContainer[i].name === name) {
+        for (var j = i; j < this.wishlistContainer.length; j++) {
+          if (i + 1 < this.wishlistContainer.length) {
+            this.wishlistContainer[j] = this.wishlistContainer[j + 1];
+          }
+        }
+        this.wishlistContainer.pop();
+        break;
+      }
+    }
+  }
+
+  this.popFromWishlistByIndex = function(index) {
+    if (index < 0 || index >= this.wishlistContainer.length) {
+      return;
+    }
+    for (var j = index; j < this.wishlistContainer.length; j++) {
+      if (j + 1 < this.wishlistContainer.length) {
+        this.wishlistContainer[j] = this.wishlistContainer[j + 1];
+      }
+    }
+
+    this.wishlistContainer.pop();
+  }
+};
+
+/**
+ * TODO
+ *       - consider a time parameter, at what interval to launch
+ *         the crawling.
+ */
+ module.exports = function(robot) {
+  robot.respond(/start crawling/i, function(message) {
+    setInterval(function() {
+      var wishlist = robot.brain.get('wishlist');
+      var completeUrl;
+
+      for (var i = 0; i < wishlist.count(); i++) {
+        if (wishlist.getElementAtIndex(i).name === "") {
+          continue;
+        }
+        completeUrl = START_URL + wishlistgetElementAtIndex(i).name;
+        computePriceForProductFromUrl(completeUrl, function(price, imgSrcUrl, productHref) {
+          if (wishlist.getElementAtIndex(i).desiredPrice >= price) {
+
+            // expand the output with the discount infos, item name, link, image,
+            // new price, etc
+            message.reply("hey, discount");
+          }
+        });
+      }
+    }, 60000);
+  });
+};
+
+module.exports = function(robot) {
+  robot.respond(/add to wishlist/i, function(message) {
+    message.reply("Ok, what do you want to consider?");
+    // TODO  Conversation
+  });
+};
+
+
 function computePriceForProductFromUrl(url, callback) {
   // Make the request
   console.log("Visiting page " + url);
