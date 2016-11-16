65c6c6722754d774b183b9ff1bdcc2627be867e1
Alex <alexandruionascu.bd@gmail.com>
2016-11-16 20:28:09 +0200

    now phineas can play good music - manele

--- scripts/good_music.js
+++ scripts/good_music.js
@@ -1,9 +1,20 @@
 'use strict';
 
+var _fs = require('fs');
+
+var _fs2 = _interopRequireDefault(_fs);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
 module.exports = function (robot) {
 
 	robot.hear(/manea/i, function (res) {
-		res.send('bagam o manea');
+		_fs2.default.readFile('src/music.json', 'utf8', function (err, data) {
+			if (err) throw err;
+			var musicList = JSON.parse(data);
+			var randomIndex = Math.floor(Math.random() * musicList.length);
+			res.send(musicList[randomIndex].url);
+		});
 	});
 
 	robot.router.post('/test', function (req, res) {
--- src/good_music.js
+++ src/good_music.js
@@ -1,9 +1,17 @@
 'use strict';
+import FileSystem from 'fs';
 
 module.exports = (robot) => {
 
 	robot.hear(/manea/i, res => {
-		res.send('bagam o manea');
+		FileSystem.readFile('src/music.json', 'utf8', function (err, data) {
+		  	if (err)
+			 		throw err;
+			 	let musicList = JSON.parse(data);
+				let randomIndex = Math.floor(Math.random() * musicList.length);
+				res.send(musicList[randomIndex].url);
+			});
+
 	});
 
 	robot.router.post('/test', (req, res) => {
