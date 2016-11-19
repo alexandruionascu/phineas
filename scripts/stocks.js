75bddabd69b22c445363d2839c60fb55f593aafd
Alex <alexandruionascu.bd@gmail.com>
2016-11-20 00:43:31 +0200

    added stock manager

--- scripts/stocks.js
+++ scripts/stocks.js
@@ -1,13 +1,67 @@
 'use strict';
 
+var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+
 var _yfinance = require('yfinance');
 
 var _yfinance2 = _interopRequireDefault(_yfinance);
 
+var _fs = require('fs');
+
+var _fs2 = _interopRequireDefault(_fs);
+
+var _natural = require('natural');
+
+var _natural2 = _interopRequireDefault(_natural);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+var COMPANIES_FILE = 'src/companies.json';
+var TEXT_ENCODING = 'utf8';
+
+var StockManager = function () {
+  function StockManager() {
+    _classCallCheck(this, StockManager);
+
+    var data = _fs2.default.readFileSync(COMPANIES_FILE, TEXT_ENCODING);
+    this._companies = JSON.parse(data);
+  }
+
+  _createClass(StockManager, [{
+    key: 'getCompany',
+    value: function getCompany(companyName) {
+      var maxDistance = 0;
+      var bestCompany = void 0;
+      this.companies.forEach(function (company) {
+        var distance = _natural2.default.JaroWinklerDistance(company.Name, companyName);
+        if (maxDistance < distance) {
+          maxDistance = distance;
+          bestCompany = company;
+        }
+      });
+
+      return bestCompany;
+    }
+  }, {
+    key: 'companies',
+    get: function get() {
+      return this._companies;
+    },
+    set: function set(values) {
+      this._companies = values;
+    }
+  }]);
+
+  return StockManager;
+}();
 
 module.exports = function (robot) {
   robot.respond(/stock for (.*)/i, function (res) {
+    var stockManager = new StockManager();
+    console.log(stockManager.getCompany(res.match[1]));
+
     _yfinance2.default.getQuotes(res.match[1], function (err, data) {
       if (err) {
         throw err;
@@ -23,19 +77,5 @@ module.exports = function (robot) {
       }
       console.log(data);
     });
-  });
-
-  robot.listen(function (message) {
-    console.log(JSON.stringify(message).text);
-    return Math.random() > 0.5;
-  }, function (response) {
-    response.reply('OMG THEY CALLED ME');
-  });
-
-  robot.listen(function (message) {
-    console.log(JSON.stringify(message.text));
-    return Math.random() < 0.2;
-  }, function (response) {
-    response.reply('OMG THEY CALLED ME IM SO RAREEE');
   });
 };
--- src/stocks.js
+++ src/stocks.js
@@ -1,7 +1,44 @@
 import YahooFinance from 'yfinance';
+import FileSystem from 'fs';
+import Natural from 'natural';
+
+const COMPANIES_FILE = 'src/companies.json';
+const TEXT_ENCODING = 'utf8';
+
+class StockManager {
+  constructor() {
+    const data = FileSystem.readFileSync(COMPANIES_FILE, TEXT_ENCODING);
+    this._companies = JSON.parse(data);
+  }
+
+  get companies() {
+    return this._companies;
+  }
+
+  getCompany(companyName) {
+    let maxDistance = 0;
+    let bestCompany;
+    this.companies.forEach((company) => {
+      const distance = Natural.JaroWinklerDistance(company.Name, companyName);
+      if (maxDistance < distance) {
+        maxDistance = distance;
+        bestCompany = company;
+      }
+    });
+
+    return bestCompany;
+  }
+
+  set companies(values) {
+    this._companies = values;
+  }
+}
 
 module.exports = (robot) => {
   robot.respond(/stock for (.*)/i, (res) => {
+    const stockManager = new StockManager();
+    console.log(stockManager.getCompany(res.match[1]));
+
     YahooFinance.getQuotes(res.match[1], (err, data) => {
       if (err) {
         throw err;
@@ -17,19 +54,5 @@ module.exports = (robot) => {
       }
       console.log(data);
     });
-  });
-
-  robot.listen((message) => {
-    console.log(JSON.stringify(message).text);
-    return Math.random() > 0.5;
-  }, (response) => {
-    response.reply('OMG THEY CALLED ME');
-  });
-
-  robot.listen((message) => {
-    console.log(JSON.stringify(message.text));
-    return Math.random() < 0.2;
-  }, (response) => {
-    response.reply('OMG THEY CALLED ME IM SO RAREEE');
   });
 };
