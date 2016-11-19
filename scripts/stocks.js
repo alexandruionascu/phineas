fc329c3c624c452ac02f0d61063645f6d41abda0
Alex <alexandruionascu.bd@gmail.com>
2016-11-19 21:58:30 +0200

    added custom listener example

--- scripts/stocks.js
+++ scripts/stocks.js
@@ -4,21 +4,38 @@ var _yfinance = require('yfinance');
 
 var _yfinance2 = _interopRequireDefault(_yfinance);
 
-var _tickertape = require('tickertape');
-
-var _tickertape2 = _interopRequireDefault(_tickertape);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 module.exports = function (robot) {
   robot.respond(/stock for (.*)/i, function (res) {
-    //const companySymbol = TickerTape.getSymbol(res.match[1]);
-    //res.send("Company name is " + companySymbol);
     _yfinance2.default.getQuotes(res.match[1], function (err, data) {
       if (err) {
         throw err;
       }
       console.log(data);
     });
+  });
+
+  robot.respond(/history for (.*)/i, function (res) {
+    _yfinance2.default.getHistorical(res.match[1], '2016-01-01', '2016-08-05', function (err, data) {
+      if (err) {
+        throw err;
+      }
+      console.log(data);
+    });
+  });
+
+  robot.listen(function (message) {
+    console.log(JSON.stringify(message));
+    return Math.random() > 0.5;
+  }, function (response) {
+    response.reply('OMG THEY CALLED ME');
+  });
+
+  robot.listen(function (message) {
+    console.log(JSON.stringify(message));
+    return Math.random() < 0.2;
+  }, function (response) {
+    response.reply('OMG THEY CALLED ME IM SO RAREEE');
   });
 };
