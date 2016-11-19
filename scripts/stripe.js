47c93bd6f949cd8daf05994b05bb955516af940b
marius92mc <marius_92m@yahoo.com>
2016-11-19 19:49:53 +0200

    Fixed stripe transactions retrieve issue

--- scripts/stripe.js
+++ scripts/stripe.js
@@ -8,29 +8,29 @@ function TransactionWithTime(amount, description, created) {
 }
 
 
-module.exports = {
-  TransactionList: function(robot) {
-    robot.hear(/show history/i, function(message) {
+module.exports = function(robot) {
 
+    robot.hear(/show history/i, function(message) {
+      /*
       var x = new StripeTransactions();
       history = x.getStripeTransactions();
-
+      console.log("aaaaaaaaaa");
       for (var i = 0; i < history.length; i++) {
         console.log(history[i].amount);
       }
-
+      */
       message.reply("Your transactions are loading...")
       stripe.balance.listTransactions(function(err, transactions) {
         var history = [];
-        for(var i = 0; i < transactions.data.length; i++) {
+        for (var i = 0; i < transactions.data.length; i++) {
           history.push(transactions.data[i].description + " " + transactions.data[i].amount + "$");
         }
-        for(var billIndex in history)
+        for (var billIndex in history) {
           message.reply(history[billIndex]);
+        }
 
-        message.reply(JSON.stringify(transactions));
+        //message.reply(JSON.stringify(transactions));
       });
-
     });
 
     robot.respond(/what is my account id/i, function(message) {
@@ -46,4 +46,3 @@ module.exports = {
       });
     });
 }
-};
