cfcdcfd7b71899a14cce67a027459d899bf1128c
balabanneculai <balabanneculai920@gmail.com>
2016-11-19 22:05:46 +0200

    making progress

--- src/luis_interpret.js
+++ src/luis_interpret.js
@@ -1,21 +1,49 @@
-import FileSystem from 'fs';
+import Request from 'request';
 
-const LUIS_URL = 'https://api.projectoxford.ai/luis/v2.0/apps/54e9b18d-25a2-4800-be59-00255000fc7a?subscription-key=f1c110e261b746f8bb2c719f059c4327&q='
+const APP_ID = '54e9b18d-25a2-4800-be59-00255000fc7a';
+const APP_SECRET = 'f1c110e261b746f8bb2c719f059c4327';
+const BASE_URL = 'https://api.projectoxford.ai/luis/v2.0/apps/';
+
+class LuisAdapter {
+  constructor(appId, appSecret) {
+    this.appId = appId;
+    this.appSecret = appSecret;
+  }
+
+  query(queryString, callback) {
+    let queryUrl = `${BASE_URL}${this.appId}`;
+    queryUrl += `?subscription-key=${this.appSecret}&q=${queryString}`;
+
+    Request(queryUrl, (err, res, body) => {
+      if (err) {
+        throw err;
+      }
+      if (res.statusCode === 200) {
+        callback(body);
+      }
+    });
+  }
+}
 
 module.exports = (robot) => {
+  robot.hear(/(.*)/i, (res) => {
+    const luisAdapter = new LuisAdapter(APP_ID, APP_SECRET);
+    luisAdapter.query(res.match[1], (data) => {
+      const jsonData = JSON.parse(data);
+      //  debug
+      res.send(jsonData.topScoringIntent.intent);
+      for (let i = 0; i < jsonData.entities.length; i += 1) {
+        res.send(jsonData.entities[i].entity);
+      }
 
-    robot.hear(/(.*)/i, res => {
-      let q = res.match[1];
+      switch (jsonData.topScoringIntent.intent) {
+        case 'Convert':
+          break;
+        case 'GetStock':
+          break;
+        default:
 
-      res.http(LUIS_URL+q).get()((err, msg, body) => {
-          let data = JSON.parse(body);
-
-          res.send(data.topScoringIntent.intent);
-
-          for (let i = 0; i < data.entities.length; i++){
-            res.send(data.entities[i].entity);
-          }
-      });
-
+      }
+    });
   });
 };
